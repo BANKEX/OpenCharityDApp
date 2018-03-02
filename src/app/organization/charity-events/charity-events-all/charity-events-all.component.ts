6e1b1167f7633bdfbf48e6d1f65f1215e7fb9875
Egor Bukraba <eb@bankexfoundation.org>
2018-03-02 16:46:36 +0300

    Add new CE form on  page

--- src/app/organization/charity-events/charity-events-all/charity-events-all.component.ts
+++ src/app/organization/charity-events/charity-events-all/charity-events-all.component.ts
@@ -7,6 +7,8 @@ import {CharityEventContractService} from '../../../core/contracts-services/char
 import {TokenContractService} from '../../../core/contracts-services/token-contract.service';
 import {CharityEventsListBaseComponent} from '../charity-events-list-base.component';
 import {MetaDataStorageService} from '../../../core/meta-data-storage.service';
+import {AppCharityEvent, ConfirmationResponse, ConfirmationStatusState} from '../../../open-charity-types';
+import {OrganizationSharedService} from '../../services/organization-shared.service';
 
 @Component({
 	selector: 'opc-charity-events-all',
@@ -17,6 +19,8 @@ export class CharityEventsAllComponent extends CharityEventsListBaseComponent im
 	public name: string = '';
 	public changer: string = 'actual';
 
+	public isAddCharityFormEnabled: boolean = false;
+
 	constructor(
 		protected organizationContractService: OrganizationContractService,
 		protected charityEventContractService: CharityEventContractService,
@@ -24,7 +28,8 @@ export class CharityEventsAllComponent extends CharityEventsListBaseComponent im
 		protected zone: NgZone,
 		protected metaDataStorageService: MetaDataStorageService,
 		private route: ActivatedRoute,
-		private router: Router
+		private router: Router,
+		private organizationSharedService: OrganizationSharedService
 	) {
 		super(organizationContractService, tokenContractService, charityEventContractService, zone, metaDataStorageService);
 	}
@@ -33,11 +38,73 @@ export class CharityEventsAllComponent extends CharityEventsListBaseComponent im
 		this.route.params.subscribe(params => { this.organizationAddress = params['address']; });
 		this.name = await this.organizationContractService.getName(this.organizationAddress);
 		this.updateCharityEventsList();
+		this.initEventsListeners();
+	}
+
+	private initEventsListeners(): void {
+		this.organizationSharedService.onCharityEventAdded()
+			.takeUntil(this.componentDestroyed)
+			.subscribe((res: AppCharityEvent) => {
+				this.charityEvents.push(merge({}, res, {raised: 0}));
+				this.hideAddCharityEventForm();
+			}, (err: any) => {
+				console.error(err);
+				alert(`Error ${err.message}`);
+			});
+
+
+		this.organizationSharedService.onCharityEventConfirmed()
+			.takeUntil(this.componentDestroyed)
+			.subscribe((res: ConfirmationResponse) => {
+				const i: number = findIndex(this.charityEvents, {internalId: res.internalId});
+				if (i !== -1) {
+					this.charityEvents[i].address = res.address;
+					this.charityEvents[i].confirmation = ConfirmationStatusState.CONFIRMED;
+				}
+			}, (err: any) => {
+				console.error(err);
+				alert(`Error ${err.message}`);
+			});
+
+		this.organizationSharedService.onCharityEventFailed()
+			.takeUntil(this.componentDestroyed)
+			.subscribe((res: ConfirmationResponse) => {
+
+				const i: number = findIndex(this.charityEvents, {internalId: res.internalId});
+				if (i !== -1) {
+					this.charityEvents[i].confirmation = ConfirmationStatusState.FAILED;
+				}
+			}, (err: any) => {
+				console.error(err);
+				alert(`Error ${err.message}`);
+			});
+
+
+		this.organizationSharedService.onCharityEventCanceled()
+			.takeUntil(this.componentDestroyed)
+			.subscribe((res: ConfirmationResponse) => {
+
+				const i: number = findIndex(this.charityEvents, {internalId: res.internalId});
+				if (i !== -1) {
+					this.charityEvents.splice(i, 1);
+				}
+			}, (err: any) => {
+				console.error(err);
+				alert(`Error ${err.message}`);
+			});
 	}
 
 	public goBackToOrganization(event: Event): void {
 		this.router.navigate(['/organization', this.organizationAddress]);
 		event.preventDefault();
+	}
+
+	public showAddCharityEventForm() {
+		this.isAddCharityFormEnabled = true;
+	}
+
+	public hideAddCharityEventForm() {
+		this.isAddCharityFormEnabled = false;
 	}
 
 	ngOnDestroy(): void {
