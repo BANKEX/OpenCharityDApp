e36e14e3e46701744f59ade28e5815a2140c71f1
Dmytro Kutsaniuk <kutsaniuk@gmail.com>
2018-03-23 16:24:54 +0200

    Added loading overlay for move funds popup

---
+++ src/app/core/loading-transparent-overlay.service.ts
@@ -0,0 +1,26 @@
+import {Injectable} from '@angular/core';
+import {Subject} from 'rxjs/Subject';
+import {Observable} from 'rxjs/Observable';
+
+@Injectable()
+export class LoadingTransparentOverlayService {
+
+	private _showOverlay = false;
+	private onOverlayStateChangedSource: Subject<boolean> = new Subject<boolean>();
+
+	constructor() {}
+
+	public showOverlay(): void {
+		this._showOverlay = true;
+		this.onOverlayStateChangedSource.next(true);
+	}
+
+	public hideOverlay(): void {
+		this._showOverlay = false;
+		this.onOverlayStateChangedSource.next(false);
+	}
+
+	public onOverlayStateChanged(): Observable<boolean> {
+		return this.onOverlayStateChangedSource.asObservable().share();
+	}
+}
