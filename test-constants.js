b8faa648cfad4d02303d2a0fa42bb57c2623afb5
Egor Bukraba <eb@bankexfoundation.org>
2018-04-05 13:36:22 +0300

    Test

--- test-constants.js
+++ test-constants.js
@@ -16,7 +16,12 @@ exports.STAGING_ENVIRONMENT = {
 	'tokenAddress': '0x6a183381d14371b4a228cca37802c09bd166ba9e',
 	'rpcProviderUrl': 'https://rpcprovider.staging.bankex.team:8635',
 	'websocketProviderUrl': 'wss://wsprovider.staging.bankex.team:8636',
-	'apiUrl': 'https://opencharity.staging.bankex.team/api/'
+	'apiUrl': 'https://opencharity.staging.bankex.team/api/',
+	'organizations': [
+		'0x0fe282ccf65c07eaeab8e829def17d8ede27447c',
+		'0x3b74105e82b5ae9ef799fb852a88a47674148f68',
+		'0xe5541c69a2e0927283e11714e01c32268ecff410'
+	]
 };
 
 exports.PROD_ENVIRONMENT = {
@@ -24,5 +29,10 @@ exports.PROD_ENVIRONMENT = {
 	'tokenAddress': '0x7487a0251a0701a89cade302679b1d01c3d8a44f',
 	'rpcProviderUrl': 'https://rpcprovider.opencharity.bankex.team:8635',
 	'websocketProviderUrl': 'wss://wsprovider.opencharity.bankex.team:8636',
-	'apiUrl': 'https://opencharity.bankex.team/api/'
+	'apiUrl': 'https://opencharity.bankex.team/api/',
+	'organizations': [
+		'0x0fe282ccf65c07eaeab8e829def17d8ede27447c',
+		'0x3b74105e82b5ae9ef799fb852a88a47674148f68',
+		'0xe5541c69a2e0927283e11714e01c32268ecff410'
+	]
 };
--- test.js
+++ test.js
@@ -2,5 +2,14 @@ const constants = require('./test-constants');
 const apiUrl = constants.STAGING_ENVIRONMENT.apiUrl;
 const env = '$CI_COMMIT_REF_NAME';
 
+if (env === 'production') {
+	fet
+
+} else if(env === 'staging') {
+
+} else if(env === 'master') {
+
+}
+
 console.log(env);
 
