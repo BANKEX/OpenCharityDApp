1eb359188d7c23f68c7ed0a08ab9986bc7722878
Egor Bukraba <eb@bankexfoundation.org>
2018-04-05 13:32:12 +0300

    Test

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 const constants = require('./test-constants');
 const apiUrl = constants.STAGING_ENVIRONMENT.apiUrl;
-const env = $CI_COMMIT_REF_NAME;
+const env = '$CI_COMMIT_REF_NAME';
 
 console.log(env);
 
