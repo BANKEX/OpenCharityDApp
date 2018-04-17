04181b18b7fd32da6f68c7d337834a33695cea06
Egor Bukraba <eb@bankexfoundation.org>
2018-04-17 17:07:23 +0300

    Add auth token to post-deploy script

--- post-deploy-script.js
+++ post-deploy-script.js
@@ -24,7 +24,8 @@ const setList = (type) => {
 			CharityEvent: charityEventAbi,
 			IncomingDonation: incomingDonationAbi,
 			OpenCharityToken: openCharityTokenAbi
-		}
+		},
+		token: (env === 'production') ? $TOKENPROD : $TOKENSTAGE
 	};
 
 	request({
