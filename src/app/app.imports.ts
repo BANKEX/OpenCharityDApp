e25e1c5f2e8a89f0ec001ba9ca20a6c8ea3cab8f
Egor Bukraba <eb@bankexfoundation.org>
2018-04-17 16:56:48 +0300

    Remove unused npm packages

--- src/app/app.imports.ts
+++ src/app/app.imports.ts
@@ -1,7 +1,6 @@
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {ReactiveFormsModule} from '@angular/forms';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-import {PerfectScrollbarModule} from 'ngx-perfect-scrollbar';
 import {OrganizationModule} from './organization/organization.module';
 import {FileDropModule} from 'ngx-file-drop';
 import {ToastyModule} from 'ng2-toasty';
@@ -9,7 +8,6 @@ import {ToastyModule} from 'ng2-toasty';
 export const APP_IMPORTS = [
 	BrowserAnimationsModule,
 	NgbModule.forRoot(),
-	PerfectScrollbarModule,
 	ReactiveFormsModule,
 	OrganizationModule,
 	FileDropModule,
